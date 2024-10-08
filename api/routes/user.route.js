137c385a9ed26e4af21a37b03c7d7ac478fc6409
erbdev <erbdev@hotmail.com>
2024-10-08 12:31:07 +0100

    create get user listings api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -3,6 +3,7 @@ import {
   test,
   updateUser,
   deleteUser,
+  getUserListings,
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -11,5 +12,6 @@ const router = express.Router();
 router.get("/test", test); // Add the test route
 router.post("/update/:id", verifyToken, updateUser); // Add the verifyToken middleware
 router.delete("/delete/:id", verifyToken, deleteUser); // Add the verifyToken middleware to the delete route
+router.get("/listings/:id", verifyToken, getUserListings); // Add the verifyToken middleware to the get user listings route
 
 export default router;
