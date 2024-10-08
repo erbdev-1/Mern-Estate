ad282c8945319d6241f4b95b0c0f20cee33cec25
erbdev <erbdev@hotmail.com>
2024-10-08 13:47:57 +0100

    create update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
   try {
@@ -20,6 +21,26 @@ export const deleteListing = async (req, res, next) => {
   try {
     await Listing.findByIdAndDelete(req.params.id); // Delete the listing by ID
     res.status(200).json("Listing has been deleted..."); // Return a success message
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id); // Find the listing by ID
+
+  if (!listing) return next(errorHandler(404, "Listing not found")); // Return an error if the listing is not found
+
+  if (req.user.id !== listing.userRef)
+    return next(errorHandler(401, "You can only update your own listings")); // Return an error if the user ID does not match the listing's userRef
+
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedListing);
   } catch (error) {
     next(error);
   }
