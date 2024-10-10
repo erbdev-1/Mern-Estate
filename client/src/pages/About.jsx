02de39f0b6d6677093f4361e756cf1fda4cf727a
erbdev <erbdev@hotmail.com>
2024-10-10 14:05:30 +0100

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,34 @@
-import React from "react";
-
 function About() {
-  return <div>About</div>;
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slade-800">
+        About RedStar Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        RedStar, founded in 2010 by Erhan B. is one of the oldest firms of
+        Estate Agents in Eastern England. Since that date the firm has evolved
+        into a dynamic and professional business with an enviable reputation,
+        remaining an independent company, and offering a personal service
+        tailor-made to meet the specific needs of each client.
+      </p>
+
+      <p className="mb-4 text-slate-700">
+        Using up-to-date technology and in-depth knowledge of local market
+        conditions, our experienced staff are confident of achieving the best
+        results possible when dealing with sales of all types of residential
+        property.
+      </p>
+
+      <p className="mb-4 text-slate-700">
+        Backed by experienced and professional staff, RedStar are confident that
+        we will remain a market leader and can offer facilities and expertise
+        for whatever your needs may be. Whether buying or selling a property,
+        finding a solicitor, arranging a survey, letting your property,
+        developing a plot of land, or a valuation of your home, call the RedStar
+        office nearest you.
+      </p>
+    </div>
+  );
 }
 
 export default About;
