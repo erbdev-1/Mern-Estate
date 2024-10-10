35e1a449c41095478c4d73a19a558b8ede2babc2
erbdev <erbdev@hotmail.com>
2024-10-10 14:19:38 +0100

    fix app.jsx file for signUp

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
-import SignUp from "./pages/Signup";
+import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
